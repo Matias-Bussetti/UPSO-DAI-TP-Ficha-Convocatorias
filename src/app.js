eebeea05d300240f4b0e07600e0fdea9a587431d
Matias-Bussetti <desarrollos.informaticos.bsst@gmail.com>
2022-06-09 17:36:22 -0300

    9-6-22 progress bar working

--- src/app.js
+++ src/app.js
@@ -8,31 +8,73 @@ xhttp.onreadystatechange = (e) => {
 };
 
 //Load
-xhttp.onloadstart = (e) => console.log("xhttp.onloadstart", e);
-xhttp.onprogress = (e) => console.log("xhttp.onprogress", e);
-xhttp.onloadend = (e) => console.log("xhttp.onloadend", e);
+xhttp.onloadstart = (e) => {
+  console.log("xhttp.onloadstart", e);
+  document.getElementById("send").setAttribute("disabled", "");
+  document.getElementById("loading-button-icon").style.visibility = "visible";
+};
+
+xhttp.onprogress = (e) => {
+  console.log("xhttp.onprogress", e);
+
+  if (window.navigator.onLine) {
+    if (document.getElementById("progress").classList.contains("bg-danger")) {
+      document.getElementById("progress").classList.remove("bg-danger");
+    }
+    document.getElementById("progress").style.width = `${Math.round(
+      (100 * e.loaded) / e.total
+    )}%`;
+    document.getElementById("progress").innerText = `${Math.round(
+      (100 * e.loaded) / e.total
+    )}%`;
+  } else {
+    if (!document.getElementById("progress").classList.contains("bg-danger")) {
+      document.getElementById("progress").classList.add("bg-danger");
+    }
+    document.getElementById("progress").style.width = "100%";
+    document.getElementById("progress").innerText =
+      "Se Perdio la Conección con el internet";
+  }
+};
+
 xhttp.onload = (e) => console.log("xhttp.onload", e);
+xhttp.onloadend = (e) => {
+  console.log("xhttp.onloadend", e);
+  document.getElementById("send").removeAttribute("disabled");
+  document.getElementById("loading-button-icon").style.visibility = "hidden";
+};
 
 //ERROR
-xhttp.onerror = (e) => console.log("xhttp.onerror", e);
+
+xhttp.onerror = (e) => {
+  console.log("xhttp.onerror", e);
+  if (!window.navigator.onLine) {
+    document.getElementById("progress").style.width = "100%";
+    document.getElementById("progress").innerText =
+      "Se Perdio la Conección con el internet";
+    document.getElementById("progress").classList.add("bg-danger");
+  }
+};
 xhttp.onabort = (e) => {
   console.log("xhttp.onabort", e);
-  const CONTROLADOR = new AbortController();
-  const SENIAL = CONTROLADOR.signal;
-  CONTROLADOR.abort();
+  document.getElementById("progress").setAttribute("aria-valuemax", e.total);
+  document.getElementById("progress").setAttribute("aria-valuenow", e.loaded);
+  document.getElementById("progress").style.width = "100%";
+  document.getElementById("progress").innerText = "Error";
 };
 
 window.onload = () => {
   document.getElementById("send").addEventListener("click", () => {
-    console.log("SEND");
+    //console.log("SEND");
     //axhttp.open("GET", "./ficha_convocatorias.xml");
     xhttp.open(
       "GET",
-      "http://aiweb.cs.washington.edu/research/projects/xmltk/xmldata/data/pir/psd7003.xml"
+      "http://aiweb.cs.washington.edu/research/projects/xmltk/xmldata/data/nasa/nasa.xml"
     );
     xhttp.send();
   });
   document.getElementById("abort").addEventListener("click", () => {
-    console.log("ABORT");
+    //console.log("ABORT");
+    xhttp.abort();
   });
 };
